be085bb4e305651a1bc3740d03227896936b8af1
Suman Banerjee <sumu.animator@gmail.com>
2025-03-28 13:26:20 +0530

    update code

---
+++ js/constants.js
@@ -0,0 +1,3 @@
+// Define the global base URL for your API
+ const BASE_URL = "https://test.api.itisiya.com:8443/b2winai/";
+//const BASE_URL = "https://test.api.itisiya.com:9443/b2winai_production/";
